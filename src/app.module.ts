9c38dcbc3dcfbf92f618d490dcb17e2e25e4effc
KevinDanielDev <k4vind@gmail.com>
2026-03-19 13:27:07 -0500

    feat: 🎸 Validation of environment variables to run the project
    Validations were added to the environment variables.
    
    ✅ Closes: #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+
+import { DatabaseModule } from './database/database.module';
+
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        PORT: Joi.number().default(3000).required(),
+      }),
     }),
     DatabaseModule,
   ],
