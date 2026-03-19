2150694fb3181ef4eac72674dd4435be696c5ba2
KevinDanielDev <k4vind@gmail.com>
2026-03-19 15:18:07 -0500

    Global API prefix + global endpoint validations

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
   logger.log(
     `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
