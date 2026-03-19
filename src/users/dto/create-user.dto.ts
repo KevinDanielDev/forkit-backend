c2d749e29adcc190db7dba29aedf71cedd90b16a
KevinDanielDev <k4vind@gmail.com>
2026-03-19 15:14:20 -0500

    Creation of user entities and DTOs.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,47 @@
-export class CreateUserDto {}
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  MaxLength,
+  MinLength,
+  IsBoolean,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(5)
+  @MinLength(2)
+  codeCountry: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
+  @MinLength(10)
+  @IsPhoneNumber()
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  @MinLength(8)
+  password: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,58 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  firstName: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  lastName: string;
+
+  @Column({
+    type: 'varchar',
+    length: 5,
+    nullable: false,
+  })
+  codeCountry: string;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    nullable: false,
+    unique: true,
+  })
+  phone: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'boolean',
+    nullable: false,
+    default: true,
+  })
+  isActive: boolean;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  password: string;
+}
